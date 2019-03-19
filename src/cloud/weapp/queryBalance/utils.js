9279946a27e16898dd6e9b8f4bbe50a29b918908
liangzch <mvpleung@gmail.com>
2019-03-19 14:26:45 +0800

    修复日期格式异常

--- src/cloud/weapp/queryBalance/utils.js
+++ src/cloud/weapp/queryBalance/utils.js
@@ -1,64 +1,8 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+moment = require('moment')
 
 axios.defaults.timeout = 60000
-
-/**
- * 日期格式化
- * @param {String} fmt 日期格式
- */
-Date.prototype.Format = function(fmt = 'yyyy-MM-dd') {
-  //author: meizz
-  let o = {
-    'M+': this.getMonth() + 1, //月份
-    'd+': this.getDate(), //日
-    'h+': this.getHours(), //小时
-    'm+': this.getMinutes(), //分
-    's+': this.getSeconds(), //秒
-    'q+': Math.floor((this.getMonth() + 3) / 3), //季度
-    S: this.getMilliseconds() //毫秒
-  }
-  if (/(y+)/.test(fmt))
-    fmt = fmt.replace(
-      RegExp.$1,
-      String(this.getFullYear()).substr(4 - RegExp.$1.length)
-    )
-  for (let k in o)
-    if (new RegExp('(' + k + ')').test(fmt))
-      fmt = fmt.replace(
-        RegExp.$1,
-        RegExp.$1.length === 1
-          ? o[k]
-          : ('00' + o[k]).substr(String(o[k]).length)
-      )
-  return fmt
-}
-
-/**
- * 格式化时间
- * @param {Number|Date} unixTime 日期或时间戳
- * @param {String} pattern 日期格式规则(如:YYYY-MM-dd)
- * @return {*}
- */
-exports.formatDate = (unixTime, pattern = 'yyyy-MM-dd') => {
-  if (unixTime instanceof Date) {
-    return unixTime.Format(pattern)
-  }
-  if (
-    Object.prototype.toString.call(unixTime) === '[object String]' &&
-    unixTime
-  ) {
-    if (unixTime.length === 8) {
-      unixTime =
-        unixTime.substring(0, 4) +
-        '-' +
-        unixTime.substring(4, 6) +
-        '-' +
-        unixTime.substring(6, 8)
-    }
-  }
-  return !unixTime || !unixTime ? '' : new Date(unixTime).Format(pattern)
-}
 
 /**
  * 获取余额信息
@@ -93,7 +37,9 @@ exports.get = async cardNumber => {
             .replace('&#x52A;', '')
         ),
         cardValidity: _$($td[2]).html(),
-        currentTime: exports.formatDate(Date.now()),
+        currentTime: moment()
+          .utcOffset(480)
+          .format('YYYY-MM-DD HH:mm:ss'),
         updateTime:
           $table
             .find('#cardRealAmt')
